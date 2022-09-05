26a35b89518c7f215463785fb4d519a6b98d309f
Heidi <lyu9287@gmail.com>
2022-09-05 13:54:25 -0400

    v2

--- script.js
+++ script.js
@@ -2,28 +2,54 @@ let dishCount = 0;
 let soapCount = 5;
 function render() {
   const numberOfDishes = document.getElementById("dishNumber");
-  numberOfDishes.innerHTML = `Number of Dirty Dishes: ${dishCount}`;
+  numberOfDishes.innerHTML = `Dirty Dishes: ${dishCount}`;
   const numberOfSoap = document.getElementById("dishSoap");
-  numberOfSoap.innerHTML = `Number of Dish Soup: ${soapCount}`;
+  numberOfSoap.innerHTML = `Dish Soup: ${soapCount}`;
+}
+function change() {
+  const image = document.getElementById("image");
+  image.src = "sinkFull.jpg";
 }
 setInterval(() => {
   dishCount += 1;
   render();
+  change();
 }, 2000);
 
-const buttonAdd = document.getElementsByClassName("add");
-const minusButton = document.getElementsByClassName("minus");
-for (let i = 0; i < minusButton.length; i++) {
-  minusButton[i].addEventListener("click", () => {
-    if (dishCount > 0 && soapCount > 0) {
-      dishCount--;
-      soapCount--;
-    }
-    render();
-  });
-}
-buttonAdd[0].addEventListener("click", () => {
+let husbandCount = 0;
+const buttonLazy = document.getElementById("lazy");
+const doDishesButton = document.getElementById("doDishes");
+const yellHusband = document.getElementById("yell");
+
+yellHusband.addEventListener("click", () => {
+  if (husbandCount < 3 && dishCount >= 3 && soapCount >= 4) {
+    husbandCount++;
+    console.log(husbandCount);
+    dishCount = dishCount - 3;
+    soapCount = soapCount - 4;
+  }
+  if (husbandCount >= 3) {
+    yellHusband.disabled = true;
+    yellHusband.style.backgroundColor = "grey";
+    alert("Too much yelling! Give him a break!");
+    setTimeout(() => {
+      yellHusband.disabled = false;
+      husbandCount = 0;
+      yellHusband.style.backgroundColor = "rgb(246, 103, 87)";
+    }, 8000);
+  }
+  render();
+});
+
+doDishesButton.addEventListener("click", () => {
+  if (dishCount > 0 && soapCount > 0) {
+    dishCount--;
+    soapCount--;
+  }
+  render();
+});
+buttonLazy.addEventListener("click", () => {
   dishCount++;
-  soapCount = soapCount + 5;
+  soapCount = soapCount + 3;
   render();
 });
